f116a24432f68dc17e90fe777ca233805a08663b
Radhey Panchal <macmini2012@Mac-mini.local>
2020-07-24 14:32:22 -0400

    json project done

---
+++ README.md
@@ -0,0 +1,38 @@
+# Setup
+
+You need to have the following installed to be able to use the code
+1. MongoDB
+2. Virtualenv
+
+## Installation
+Create virtual environment and activate
+
+```bash
+   virtualenv venv -p python3
+   source venv/bin/activate
+```
+
+Install the dependencies
+```bash
+pip install -r requirements.txt
+```
+
+Run the web app
+```bash
+python web.py
+```
+
+### API endpoints
+
+- All customers - ```/api/customers```
+- Movies customer rented - ```/api/customers/<customer_id>```
+- All films - ```/api/films```
+- Customers who rented a film - ```/api/customers/films/<film_id>```
+
+### Tables to illustrate can be access via browser
+These tables data is loaded via JSON using jquery
+
+- All customers - ```http://127.0.0.1:5000/customers```
+- Customer rentals ```http://127.0.0.1:5000/customer_rentals```
+- Film details with list of customers who rented it ```http://127.0.0.1:5000/film_customer_rentals``` 
+
---
+++ web.py
@@ -0,0 +1,124 @@
+from flask import Flask, render_template
+from flask import jsonify
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = "DVDRental"
+app.config['MONGO_URI'] = "mongodb://localhost:27017/DVDRental"
+
+mongo = PyMongo(app)
+
+
+@app.route('/customers')
+def customers():
+    return render_template('customers.html')
+
+
+@app.route('/customer_rentals')
+def customer_rentals():
+    return render_template('customer_rentals.html')
+
+
+@app.route('/film_customer_rentals')
+def film_customer_rentals():
+    return render_template('film_customer.html')
+
+
+@app.route('/api/customers', methods=['GET'])
+def api_customers():
+    customers = mongo.db.customers
+
+    data = []  # Create a list of data we want to expose to the API
+    for customer in customers.find():
+        data.append({'id': customer['_id'],
+                     'first_name': customer['First Name'],
+                     'last_name': customer['Last Name'],
+                     'phone': customer['Phone'],
+                     'address': customer['Address'],
+                     'district': customer['District'],
+                     'country': customer['Country'],
+                     'city': customer['City'],
+                     'rentals': customer['Rentals']})
+
+    return jsonify(data)
+
+
+@app.route('/api/customers/<customer_id>', methods=['GET'])
+def api_customer(customer_id):
+    customer_id = int(customer_id)
+    customer = mongo.db.customers.find_one({'_id': customer_id})
+    if customer:
+        rentals = customer['Rentals']
+        # Create a list for the customer rental details
+        customer_rentals = []
+        for rental in rentals:
+            rental_duration = mongo.db.films.find_one({'_id': rental['filmId']})['Rental Duration']
+            customer_rentals.append(
+                {'rental_date': rental['Rental Date'], 'rental_duration': rental_duration,
+                 'rental_cost': rental['Payments'][0]['Amount']})
+        data = customer_rentals
+    else:
+        data = "No results"
+    return jsonify(data)
+
+
+@app.route('/api/films', methods=['GET'])
+def api_films():
+    films = mongo.db.films
+
+    data = []  # Create a list of data we want to expose to the API
+    for film in films.find():
+        data.append({'film': {'title': film['Title'],
+                              'category': film['Category'],
+                              'description': film['Description'],
+                              'rating': film['Rating'],
+                              'rental_duration': film['Rental Duration']}})
+
+    return jsonify(data)
+
+
+@app.route('/api/customers/films/<film_id>', methods=['GET'])
+def api_film(film_id):
+    """
+    API end point to get customers who have rented a specific film
+    :param film_id:
+    :return: film_details and customer details json
+    """
+    data = {}
+    film_id = int(film_id)
+
+    # Get the film details and add to the data that will be return by API
+    film = mongo.db.films.find_one({'_id': film_id})
+    film_details = []
+    film_details.append({'title': film['Title'],
+                         'category': film['Category'],
+                         'description': film['Description'],
+                         'length': film['Length'],
+                         'rating': film['Rating'],
+                         'rental_duration': film['Rental Duration'],
+                         'replacement_cost': film['Replacement Cost'],
+                         'special_features': film['Special Features']})
+    data['film_details'] = film_details
+
+    # get all customers then match filmId with customers' rental filmId
+    customer_rentals = []
+    customers = mongo.db.customers
+    for customer in customers.find():
+        for rental in customer['Rentals']:
+            if film_id == rental['filmId']:
+                customer_rentals.append({'id': customer['_id'],
+                                         'first_name': customer['First Name'],
+                                         'last_name': customer['Last Name'],
+                                         'phone': customer['Phone'],
+                                         'address': customer['Address'],
+                                         'district': customer['District'],
+                                         'country': customer['Country'],
+                                         'city': customer['City']})
+    data['customers'] = customer_rentals
+
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
